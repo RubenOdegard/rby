39a177a8c650105813340b15d661813b3c210dc6
Ruben <53976198+RubenOdegard@users.noreply.github.com>
2024-01-08 19:32:25 +0100

    Added dynamic copyright tag and visual changes to the name text.

--- src/components/layout-footer.tsx
+++ src/components/layout-footer.tsx
@@ -6,11 +6,12 @@ import LanguageIcon from "@/components/ui/svg/language-icon";
 import FeatureDisabledDialog from "@/components/modal-feature-disabled";
 
 export default function LayoutFooter() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="flex w-full border-t py-8 lg:py-12 px-4 md:px-12  lg:px-0">
       <div className="grid grid-cols-1 md:grid-cols-3 w-full gap-y-2 max-w-4xl mx-auto items-center justify-between px-4 lg:p-0 flex-col md:flex-row">
-        <div className="hidden md:flex uppercase order-1 md:order-0 md:justify-self-start justify-self-center font-bold ">
-          Ruben Odegard
+        <div className="hidden md:flex uppercase order-1 md:order-0 md:justify-self-start justify-self-center text-xs text-muted-foreground">
+          &copy; Ruben Odegard {currentYear}
         </div>
         <div className="flex flex-col gap-y-2 justify-self-center justify-center items-center order-0 md:order-1">
           <div className="relative">
@@ -21,7 +22,7 @@ export default function LayoutFooter() {
           <FeatureDisabledDialog>
             <ToggleLanguage />
           </FeatureDisabledDialog>
-          <h4 className="text-muted-foreground">me@rubenodegard.com</h4>
+          <h4 className="text-muted-secondary">me@rubenodegard.com</h4>
         </div>
         <div className="flex gap-2 order-2 md:order-2 mt-2 justify-self-center md:justify-self-end">
           <Link href="https://linkedin.com/in/rubenodegard" target="_blank">
